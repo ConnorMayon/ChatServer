b691e2a18aab5d76380254a9ec4c736e86f14d0c
JackMorris489 <129555314+JackMorris489@users.noreply.github.com>
2024-04-23 19:04:28 -0700

    Update main.c

--- server/main.c
+++ server/main.c
@@ -9,11 +9,22 @@
 int main()
 {
 	// initialize varaibles
-	int serverSocket;                 // descriptor of server socket
+	int serverSocket;			// descriptor of server socket
+	int clientSocket;			// descriptor of the client socket
 	struct sockaddr_in serverAddress; // for naming the server's listening socket
-	int yes = 1;
 	char* properties_file = "PROPERTIES.properties";
-    Properties* properties;
+	Properties* properties;
+	pthread_t thread;
+	int yes = 1;
+	
+	// create chat node list
+	ChatNodeLL *chatroomList;
+	
+	// create chat node bounds
+	ChatNodeBounds bounds = create_chat_node_bounds();
+
+	// read properties
+	Properties* properties = property_read_properties(properties_file);
 
 	// ignore SIGPIPE, sent when client disconnected
 	signal(SIGPIPE, SIG_IGN);
@@ -24,18 +35,18 @@ int main()
 		perror("Error creating socket");
 		exit(EXIT_FAILURE);
 	}
-
+	
 	// lose the pesky "Address already in use" error message
 	if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) 
 	{
 		perror("setsockopt");
 		exit(EXIT_FAILURE);
 	}
-
+	
 	// bind the socket
-	serverAddress.sin_family      = AF_INET;           // accept IP addresses
-	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY); // accept clients on any interface
-	serverAddress.sin_port        = htons(PORT);       // port to listen on
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+	serverAddress.sin_port = htons(SERVER_PORT);
 
 	// binding unnamed socket to a particular port
 	if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) != 0) 
@@ -45,7 +56,7 @@ int main()
 	}
 
 	// listen on the socket
-	if (listen(serverSocket, NUM_CLIENTS) != 0)
+	if (listen(serverSocket, 10) != 0)
 	{
 		perror("Error listening on socket");
 		exit(EXIT_FAILURE);
@@ -54,32 +65,15 @@ int main()
 	// have the server print that it's ready
 	printf("Ready for Connections\n");
 
-	// read properties
-	Properties* properties = property_read_properties(properties_file);
-	
-	// server loop
-	while (true)
+	// infinite loop for server
+	while(true)
 	{
-		// accept connection to client
-		int clientSocket = accept(serverSocket, NULL, NULL);
+		clientSocket = accept(serverSocket, NULL, NULL);
 		printf("\nServer with PID %d: accepted client\n", getpid());
 
-		// create thread to handle the client's request
-		// note that this is a naive approach, i.e. there are race conditions
-		// for now this is okay, assuming low load
-		pthread_t thread;
-		if (pthread_create(&thread, NULL, handleClient, (void*)&clientSocket) != 0)
-		{
-			perror("Error creating thread");
-			exit(EXIT_FAILURE);
-		}
-
-		// detach the thread so that we don't have to wait (join) with it to reclaim memory.
-		// memory will be reclaimed when the thread finishes.
-		if (pthread_detach(thread) != 0)
-		{
-			perror("Error detaching thread");
-			exit(EXIT_FAILURE);
-		}
+		// handle the client
+		pthread_create(&thread, NULL, handleClient, (void*)&clientSocket);
 	}
+	
+	return EXIT_SUCCESS;
 }
