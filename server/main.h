13c55b79220ba1c60353b25fbbc1f17f8f0a8421
JackMorris489 <129555314+JackMorris489@users.noreply.github.com>
2024-04-23 20:10:15 -0700

    Update main.h

--- server/main.h
+++ server/main.h
@@ -1,33 +1,13 @@
 // precompiler directives
+#ifndef SERVER_MAIN_H
+#define SERVER_MAIN_H
 
 // libraries
 #include "client_handler.h"
-#include "chat_node.h"
-#include "message.h"
-#include "properties.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <time.h>
-
-// socket/bind/listen/accept
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-
-// thread
-#include <pthread.h>
-#include <unistd.h> 
-
-// read/write/close
-#include <sys/uio.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <signal.h>
 
 // preprocessor directives
 #define SERVER_ADDR "142.11.199.119"
 #define PORT 23658
 #define THREAD_COUNT 2
+
+#endif
