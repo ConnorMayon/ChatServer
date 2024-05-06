3576567f18a0e6beb6a0363021a2754040e83661
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-05-05 20:23:20 -0700

    Add files via upload

--- client/receiver_handler.c
+++ client/receiver_handler.c
@@ -18,44 +18,50 @@ void *runReceiving()
             {
             // reads server output
             receive_message_from_server(clientSocket, receiveMessage);
-	    ChatNode thisNode = receiveMessage->chat_node;
-            switch(receiveMessage->message_type)
-                {
-                // translate control code
-                case JOIN:
-                // if code is J
-                    // print JOIN
-                    printf(JOINED_COLOR);
-                    printf("%s joined chat", thisNode.log_name);
-		    printf(RESET_COLOR);
-		    printf("\n");
-                break;
-                case LEAVE:
-                // if code is L
-                    // print LEAVE
-                    printf(LEFT_COLOR);
-                    printf("%s left chat", thisNode.log_name);
-		    printf(RESET_COLOR);
-		    printf("\n");
-                break;
-                case NOTE:
-                // if code is N
-                    // print message
-                    printf(NOTE_COLOR);
-                    receiveMessage->note[strlen(receiveMessage->note)-1] = '\0';
-                    printf("%s:", thisNode.log_name);
-                    printf("%s", receiveMessage->note);
-		    printf(RESET_COLOR);
-		    printf("\n");
-                break;
-                case SHUTDOWN_ALL:
-                // if code is S
-                    // print message
-                    printf("Shutting down");
-                    // run shutdown
-                    exit(EXIT_SUCCESS);
-                break;
-                }
+            if(clientConnected)
+            {
+            
+
+		    ChatNode thisNode = receiveMessage->chat_node;
+		    printf("%s", thisNode.log_name);
+		    switch(receiveMessage->message_type)
+		        {
+		        // translate control code
+		        case JOIN:
+		        // if code is J
+		            // print JOIN
+		            printf(JOINED_COLOR);
+		            printf("%s joined chat", thisNode.log_name);
+			    printf(RESET_COLOR);
+			    printf("\n");
+		        break;
+		        case LEAVE:
+		        // if code is L
+		            // print LEAVE
+		            printf(LEFT_COLOR);
+		            printf("%s left chat", thisNode.log_name);
+			    printf(RESET_COLOR);
+			    printf("\n");
+		        break;
+		        case NOTE:
+		        // if code is N
+		            // print message
+		            printf(NOTE_COLOR);
+		            receiveMessage->note[strlen(receiveMessage->note)-1] = '\0';
+		            printf("%s:", thisNode.log_name);
+		            printf("%s", receiveMessage->note);
+			    printf(RESET_COLOR);
+			    printf("\n");
+		        break;
+		        case SHUTDOWN_ALL:
+		        // if code is S
+		            // print message
+		            printf("Shutting down");
+		            // run shutdown
+		            exit(EXIT_SUCCESS);
+		        break;
+		        }
+		  }
             
             }
         }
