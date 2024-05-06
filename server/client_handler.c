67713b9f06bb6acb3a893c11e94d6cbd5e3b7dea
JackMorris489 <129555314+JackMorris489@users.noreply.github.com>
2024-05-05 22:11:49 -0700

    Update client_handler.c

--- server/client_handler.c
+++ server/client_handler.c
@@ -14,7 +14,7 @@ void* talk_to_client(void *_args)
 	ChatNodeLL *headPtr = args->chatroomList;
 
 	// lock mutex
-	pthread_mutex_unlock(&clients_mutex);
+	pthread_mutex_unlock(&clientsMutex);
 
 	// create a pointer for the chatroom list
 	ChatNodeLL *ptr = headPtr;
@@ -35,7 +35,7 @@ void* talk_to_client(void *_args)
 		ptr = headPtr;
 		
 		// determine what identifier was sent in the message and start switch statement
-		switch(inputMessage->message_type)
+		switch(inputMessage->messageType)
 		{
 			// if the JOIN identifier was sent
 			case JOIN:
@@ -44,36 +44,36 @@ void* talk_to_client(void *_args)
 				
 				// connect the client to the chatroom by adding them to the chat node.
 					// create the new chat node to add later
-				ChatNode* newNode = create_chat_node(inputMessage->chat_node.ip, inputMessage->chat_node.port_num, inputMessage->chat_node.log_name);
+				ChatNode* newNode = create_chat_node(inputMessage->chatNode.ip, inputMessage->chatNode.portNum, inputMessage->chatNode.logName);
 				
 					// add the new node's thread number manually
-				newNode->thread_num = clientSocket;
+				newNode->threadNum = clientSocket;
 				
 					// add the new clinet to the chat node list
 				add_chat_node(ptr, newNode);
 				
 				
 					// set the new node as the first bounds if not already set
-				if(args->bounds->first_node == NULL) args->bounds->first_node = newNode;
+				if(args->bounds->firstNode == NULL) args->bounds->firstNode = newNode;
 				
 					// otherwise set the new node as the last bounds
-				else args->bounds->last_node = newNode;
+				else args->bounds->lastNode = newNode;
 
 				// form the message struct so we can send a message to all the other clients
 				outputMessage = create_message(JOIN, inputMessage->note, newNode);
 
 				// send the join message to the chatroom for the requesting client
-				while(ptr->next_node != NULL)
+				while(ptr->nextNode != NULL)
 				{
     					// go to the next chat node
-					ptr = ptr->next_node;
+					ptr = ptr->nextNode;
 					
 					// if the selected chat node doesn't equal the newly created node
-					if(ptr->chat_node != newNode)
+					if(ptr->chatNode != newNode)
 					{
 						
 						// write the message to the current chat node
-						send_message_to_server(ptr->chat_node->thread_num, outputMessage);
+						send_message_to_server(ptr->chatNode->threadNum, outputMessage);
 					}
 				}
 				
@@ -84,38 +84,38 @@ void* talk_to_client(void *_args)
 			case LEAVE:
 				
 				// move past the null head
-				ptr = ptr->next_node;
+				ptr = ptr->nextNode;
 
 				// determine sender
-				while(strcmp(ptr->chat_node->log_name, inputMessage->chat_node.log_name) != 0)
+				while(strcmp(ptr->chatNode->logName, inputMessage->chatNode.logName) != 0)
 				{
 					// go to the next chat node
-					ptr = ptr->next_node;
+					ptr = ptr->nextNode;
 				}
 				
 				// form the message struct so we can send a message to all the other clients
-				outputMessage = create_message(LEAVE, inputMessage->note, &inputMessage->chat_node);
+				outputMessage = create_message(LEAVE, inputMessage->note, &inputMessage->chatNode);
 
 				// reset the pointer to the head.
 				ptr = headPtr;
 
 				// send the leave message to the rest of the chatroom
-				while(ptr->next_node != NULL)
+				while(ptr->nextNode != NULL)
 				{
 					// go to the next chat node
-					ptr = ptr->next_node;
+					ptr = ptr->nextNode;
 					
 					// if the selected chat node is not the leaving node
-					if(strcmp(ptr->chat_node->log_name, inputMessage->chat_node.log_name) != 0 )
+					if(strcmp(ptr->chatNode->logName, inputMessage->chatNode.logName) != 0 )
 					{
 						
 						// write the message to the current chat node
-						send_message_to_server(ptr->chat_node->thread_num, outputMessage);
+						send_message_to_server(ptr->chatNode->threadNum, outputMessage);
 					}
 				}
 				
 				// remove the chat node
-				remove_chat_node(headPtr, &ptr->chat_node);
+				remove_chat_node(headPtr, &ptr->chatNode);
 				
 				// close the thread between the sender and server
 				close(clientSocket);
@@ -125,16 +125,16 @@ void* talk_to_client(void *_args)
 			case SHUTDOWN_ALL:
 				
 				// form the message struct so we can send a message to all the other clients before shutting down
-				outputMessage = create_message(SHUTDOWN_ALL, inputMessage->note, &inputMessage->chat_node);
+				outputMessage = create_message(SHUTDOWN_ALL, inputMessage->note, &inputMessage->chatNode);
 				
 				// send the leave message to the entire chatroom
-				while(ptr->next_node != NULL)
+				while(ptr->nextNode != NULL)
 				{
 					// go to the next chat node
-					ptr = ptr->next_node;
+					ptr = ptr->nextNode;
 					
 					// write the message to the current chat node
-					send_message_to_server(ptr->chat_node->thread_num, outputMessage);
+					send_message_to_server(ptr->chatNode->threadNum, outputMessage);
 				}
 				
 				// shutdown the server
@@ -144,19 +144,19 @@ void* talk_to_client(void *_args)
 			case NOTE:
 				
 				// form the message struct so we can send the note to all the other clients before shutting down
-				outputMessage = create_message(NOTE, inputMessage->note, &inputMessage->chat_node);
+				outputMessage = create_message(NOTE, inputMessage->note, &inputMessage->chatNode);
 
 				// send the message to everyone but the sender
-				while(ptr->next_node != NULL)
+				while(ptr->nextNode != NULL)
 				{
 					// move to the next node
-					ptr = ptr->next_node;
+					ptr = ptr->nextNode;
 					
 					// make sure the current node isn't the sender
-					if(strcmp(ptr->chat_node->log_name, inputMessage->chat_node.log_name) != 0)
+					if(strcmp(ptr->chatNode->logName, inputMessage->chatNode.logName) != 0)
 					{
 						// write the message to the current chat node
-						send_message_to_server(ptr->chat_node->thread_num, outputMessage);
+						send_message_to_server(ptr->chatNode->threadNum, outputMessage);
 					}
 				}
 				
