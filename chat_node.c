b306f1e08f341459d50bc2b9e7d9d2b8d5e35a65
JackMorris489 <129555314+JackMorris489@users.noreply.github.com>
2024-05-05 22:12:53 -0700

    Update chat_node.c

--- chat_node.c
+++ chat_node.c
@@ -1,75 +1,74 @@
 #include "chat_node.h"
 
-void add_chat_node(ChatNodeLL* chat_node_ll, ChatNode* insertion_node) {
-    ChatNodeLL* temp_node = chat_node_ll;
-    if (chat_node_ll == NULL)
+void add_chat_node(ChatNodeLL* chatNodeLL, ChatNode* insertionNode) {
+    ChatNodeLL* tempNode = chatNodeLL;
+    if (chatNodeLL == NULL)
     {
-        chat_node_ll = insertion_node;
+        chatNodeLL = insertionNode;
         return;
     }
     // Iterate through each chat node in linked list until the next node is NULL
-    while(temp_node->next_node != NULL) temp_node = temp_node->next_node;
+    while(tempNode->nextNode != NULL) tempNode = tempNode->nextNode;
 
     // Set the next node pointer equal to the insertion node
-    temp_node->next_node = (ChatNodeLL*) malloc(sizeof(ChatNodeLL));
-    temp_node = temp_node->next_node;
-    temp_node->chat_node = insertion_node;
+    tempNode->nextNode = (ChatNodeLL*) malloc(sizeof(ChatNodeLL));
+    tempNode = tempNode->nextNode;
+    tempNode->chatNode = insertionNode;
 
 }
 
 ChatNode* create_chat_node(char* ip, int port_num, char* log_name) {
     // Allocate memory for chat node struct
-    ChatNode* new_node = (ChatNode*) malloc(sizeof(ChatNode));
+    ChatNode* newNode = (ChatNode*) malloc(sizeof(ChatNode));
 
     // Assign parameter values to newly created chat node struct
-    strcpy(new_node->ip, ip);
-    new_node->port_num = port_num;
-    strcpy(new_node->log_name, log_name);
+    strcpy(newNode->ip, ip);
+    newNode->port_num = port_num;
+    strcpy(newNode->log_name, log_name);
 
     // Return struct
-    return new_node;
+    return newNode;
 }
 
 ChatNodeBounds* create_chat_node_bounds() {
     // Allocate memory for chat node bounds struct
-    ChatNodeBounds* new_node_bounds = (ChatNodeBounds*)malloc(sizeof(ChatNodeBounds));
+    ChatNodeBounds* newNode_bounds = (ChatNodeBounds*)malloc(sizeof(ChatNodeBounds));
 
-    new_node_bounds->first_node = NULL;
-    new_node_bounds->last_node = NULL;
+    newNode_bounds->firstNode = NULL;
+    newNode_bounds->lastNode = NULL;
 
     // Return pointer to newly allocated memory
-    return new_node_bounds;
+    return newNode_bounds;
 }
 
-void remove_chat_node(ChatNodeLL* chat_node_ll, ChatNode* deletion_node) {
+void remove_chat_node(ChatNodeLL* chatNodeLL, ChatNode* deletionNode) {
     
-    ChatNodeLL* after_node, *free_node;
-    ChatNodeLL* temp_node = chat_node_ll;
-    ChatNodeLL* next_node = temp_node->next_node;
+    ChatNodeLL* afterNode, *freeNode;
+    ChatNodeLL* tempNode = chatNodeLL;
+    ChatNodeLL* nextNode = tempNode->nextNode;
 
  
     // Iterate through each chat node in linked list until the next node is the deletion node
-    while (next_node->chat_node != deletion_node) 
+    while (nextNode->chatNode != deletionNode) 
     {
-        temp_node = next_node;
-        next_node = temp_node->next_node;
+        tempNode = nextNode;
+        nextNode = tempNode->nextNode;
 
         // Assume deletion node does not exist
-        if (next_node == NULL) 
+        if (nextNode == NULL) 
         {
         return;
         }
     }
     // Store pointer to next node in a variable
-    after_node = next_node->next_node;
+    afterNode = nextNode->nextNode;
 
     // Set next node pointer equal to the deletion node's next node pointer
-    temp_node->next_node = after_node;
+    tempNode->nextNode = afterNode;
 
-    free(next_node);
+    free(nextNode);
 
 
     // Free deletion node
-    free(deletion_node);
+    free(deletionNode);
 }
-
